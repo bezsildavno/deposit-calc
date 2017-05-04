381b9356a668c903bab4f363281c00e814af904b
bezsildavno <sadbrown29@gmail.com>
2017-05-04 21:24:20 +0700

    Input check implemented

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
-int main(){
-int length, dep_amount;
+int data(int length)
+{
+	if ((length >= 0) && (length <= 365)) return 1;
+	else return 0;
+}
+int money(int dep_amount)
+{
+	if (dep_amount >= 10000) return 1;
+	else return 0;
+}
+
+int main() {
+int length, dep_amount, n;
 printf("enter lenght to deposit: \n");
 scanf("%d", &length);
 printf("enter dep_amount to deposit: \n");
 scanf("%d", &dep_amount);
-
+n = data(length);
+if (n==0) printf("Error: Incorrect length. \n");
+n=money(dep_amount)
+if (n==0) printf("Error: Incorrect dep_amount \n"); 
 
 
 
