704f3c682971679601d9f86ad0bc35763a21138f
bezsildavno <sadbrown29@gmail.com>
2017-05-04 21:44:38 +0700

    Added net profit conclusion

--- main.c
+++ main.c
@@ -44,17 +44,16 @@ int money(int dep_amount)
 
 int main() {
 int length, dep_amount, n;
-printf("enter lenght to deposit: \n");
-scanf("%d", &length);
-printf("enter dep_amount to deposit: \n");
-scanf("%d", &dep_amount);
-n = data(length);
-if (n==0) printf("Error: Incorrect length. \n");
-n=money(dep_amount);
-if (n==0) printf("Error: Incorrect dep_amount \n"); 
-printf("dep_amount at the end of: ");
-printf("%d \n", amount_of_income(length, dep_amount));
-
-
+	printf("enter lenght to deposit: \n");
+	scanf("%d", &length);
+	printf("enter dep_amount to deposit: \n");
+	scanf("%d", &dep_amount);
+		n = data(length);
+	if (n==0) printf("Error: Incorrect length. \n");
+		n=money(dep_amount);
+	if (n==0) printf("Error: Incorrect dep_amount \n"); 
+	printf("dep_amount at the end of: ");
+	printf("%d \n", amount_of_income(length, dep_amount));
+	printf("net profit: %d \n", amount_of_income(length, dep_amount)-dep_amount);
 return 0;
 }
