bdddc177d061ea2f91acec0c4411fe05c78b0eb9
bezsildavno <sadbrown29@gmail.com>
2017-05-04 21:30:45 +0700

    Calculations implemented

--- main.c
+++ main.c
@@ -1,3 +1,35 @@
+
+int amount_of_income(int length, int dep_amount) {
+	int cashback;
+
+	if ((length > 0) && (length <= 30)) {
+	cashback = dep_amount + (dep_amount / 100 * -10);
+	}
+
+	if ((length >= 31) && (length <= 120)) {
+	if (dep_amount < 100000)
+		cashback = dep_amount + (dep_amount / 100 * 2);
+	else
+		cashback = dep_amount + (dep_amount / 100 * 3);
+	}
+
+	if ((length >= 121) && (length <= 240)) {
+	if (dep_amount < 100000)
+		cashback = dep_amount + (dep_amount / 100 * 6);
+	else
+		cashback = dep_amount + (dep_amount / 100 * 8);
+	}
+
+	if ((length >= 241) && (length <= 365)) {
+	if (dep_amount < 100000)
+		cashback = dep_amount + (dep_amount / 100 * 12);
+	else
+		cashback = dep_amount + (dep_amount / 100 * 15);
+	}
+
+	return cashback;
+}
+
 int data(int length)
 {
 	if ((length >= 0) && (length <= 365)) return 1;
@@ -19,8 +51,8 @@ n = data(length);
 if (n==0) printf("Error: Incorrect length. \n");
 n=money(dep_amount)
 if (n==0) printf("Error: Incorrect dep_amount \n"); 
-
-
+printf("dep_amount at the end of: ");
+printf("%d \n", amount_of_income(length, dep_amount));
 
 
 return 0;
