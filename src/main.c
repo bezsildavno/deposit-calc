66570330b6faba84062b16058fd083550aff730e
rasputin <martapopa1978@gmail.com>
2017-05-25 09:18:00 +0700

    Replace function checkdata

--- src/main.c
+++ src/main.c
@@ -2,19 +2,18 @@
 #include "deposit.h"
 
 int main() {
-	int length, dep_amount, n;
+	int length, dep_amount;
 	printf("enter lenght to deposit: \n");
 	scanf("%d", &length);
 	printf("enter dep_amount to deposit: \n");
 	scanf("%d", &dep_amount);
-	n = data(length);
-	if (n==0) 
-		printf("Error: Incorrect length. \n");
-	n=money(dep_amount);
-	if (n==0) 
-		printf("Error: Incorrect dep_amount \n"); 
-	printf("dep_amount at the end of: ");
-	printf("%d \n", amount_of_income(length, dep_amount));
-	printf("net profit: %d \n", amount_of_income(length, dep_amount)-dep_amount);
+	if (checkdata(length, dep_amount)==0)
+       		printf("Error, please enter correct data! \n");
+	else
+	{
+		printf("dep_amount at the end of: ");
+		printf("%d \n", amount_of_income(length, dep_amount));
+		printf("net profit: %d \n", amount_of_income(length, dep_amount)-dep_amount);
+	}
 	return 0;
 }
